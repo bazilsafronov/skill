c3eafbb0e2fe1ad6d902404a8acfc4930b99d939
Bazil <bazil.safronov@gmail.com>
2024-01-22 17:08:49 +0300

    init app

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ export function Header() {
+    return (
+        <div>
+            dsfmdslkfjklds
+            <h1>Header blsdlgsdl asdlsad</h1>
+        </div>
+    )
+}
+
